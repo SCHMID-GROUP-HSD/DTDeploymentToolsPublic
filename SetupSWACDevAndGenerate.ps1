09a2428a512b14e0e7a6afee0b20e60573646d70
Robert Preis <preis.ro@schmid-group.com>
2025-05-06 07:52:53 +0200

    updated via DTDeploymentTools/deployment/public/generate.ps1

--- SetupSWACDevAndGenerate.ps1
+++ SetupSWACDevAndGenerate.ps1
@@ -12,14 +12,22 @@ function restartPwsh {
   exit $lastexitcode
 }
 
+$pwshInstalled=$false
+
 if ($null -eq (get-command pwsh -erroraction SilentlyContinue)) {
+  $ErrorActionPreference = "Stop"
+  $ProgressPreference = 'SilentlyContinue'
   $tmpPwshFile = "$env:TEMP\pwsh.msi"
   Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/PowerShell-7.4.1-win-x64.msi" -OutFile $tmpPwshFile
   ###Start-Process msiexec.exe -Wait -ArgumentList "/i `"$tmpPwshFile`" /qn /norestart"
   msiexec.exe /i $tmpPwshFile /qn /norestart | out-string -stream
   Remove-item $tmpPwshFile -erroraction SilentlyContinue
   if(0 -ne $lastexitcode) { throw "error. please see above" }
+  $pwshInstalled = $true
+}
 
+
+if($pwshInstalled) {
   restartPwsh
 }
 
