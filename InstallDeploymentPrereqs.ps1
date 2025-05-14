47a453feeeec588e0778f222d7e470290a59aa6b
Robert Preis <preis.ro@schmid-group.com>
2025-05-14 09:50:40 +0200

    updated via DTDeploymentTools/deployment/public/generate.ps1

--- InstallDeploymentPrereqs.ps1
+++ InstallDeploymentPrereqs.ps1
@@ -3,7 +3,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 function restartPwsh {
   & "C:\Program Files\PowerShell\7\pwsh.exe" -executionpolicy unrestricted -encodedcommand "UwBlAHQALQBFAHgAZQBjAHUAdABpAG8AbgBQAG8AbABpAGMAeQAgAEIAeQBwAGEAcwBzACAALQBTAGMAbwBwAGUAIABQAHIAbwBjAGUAcwBzACAALQBGAG8AcgBjAGUAOwAgAFsAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFMAZQByAHYAaQBjAGUAUABvAGkAbgB0AE0AYQBuAGEAZwBlAHIAXQA6ADoAUwBlAGMAdQByAGkAdAB5AFAAcgBvAHQAbwBjAG8AbAAgAD0AIABbAFMAeQBzAHQAZQBtAC4ATgBlAHQALgBTAGUAcgB2AGkAYwBlAFAAbwBpAG4AdABNAGEAbgBhAGcAZQByAF0AOgA6AFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB0AG8AYwBvAGwAIAAtAGIAbwByACAAMwAwADcAMgA7ACAAaQBlAHgAIAAoACcAJABwAHMAcwBjAHIAaQBwAHQAcgBvAG8AdAA9ACIAJwArACQAcABzAHMAYwByAGkAcAB0AHIAbwBvAHQAKwAnACIAOwAnACsAKAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABTAHkAcwB0AGUAbQAuAE4AZQB0AC4AVwBlAGIAQwBsAGkAZQBuAHQAKQAuAEQAbwB3AG4AbABvAGEAZABTAHQAcgBpAG4AZwAoACcAaAB0AHQAcABzADoALwAvAHIAYQB3AC4AZwBpAHQAaAB1AGIAdQBzAGUAcgBjAG8AbgB0AGUAbgB0AC4AYwBvAG0ALwBTAEMASABNAEkARAAtAEcAUgBPAFUAUAAtAEgAUwBEAC8ARABUAEQAZQBwAGwAbwB5AG0AZQBuAHQAVABvAG8AbABzAFAAdQBiAGwAaQBjAC8AcgBlAGYAcwAvAGgAZQBhAGQAcwAvAG0AYQBpAG4ALwBTAGUAdAB1AHAAUwBXAEEAQwBEAGUAdgBBAG4AZABHAGUAbgBlAHIAYQB0AGUALgBwAHMAMQAnACkAKQApAA==" | out-string -stream
-  exit $lastexitcode
+  #exit $lastexitcode
 }
 
 $pwshInstalled=$false
@@ -23,40 +23,44 @@ if($pwshInstalled) {
   restartPwsh
 }
 
-if($null -eq (get-command git -erroraction SilentlyContinue)) {
-  $tmpExeFile =  "$env:TEMP\git-setup.exe"
-  Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe" -OutFile $tmpExeFile
-  ### Start-Process -FilePath $tmpExeFile -ArgumentList "/SILENT", "/NORESTART", "/DIR=C:\Program Files\Git" -Wait -NoNewWindow
-  & $tmpExeFile "/SILENT" "/NORESTART" "/DIR=C:\Program Files\Git" | out-string -stream
-  Remove-item $tmpExeFile -erroraction SilentlyContinue
-  if(0 -ne $lastexitcode) { throw "error. please see above" }
-  $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
-}
+if ($PSVersionTable.PSEdition -eq 'Core') { 
+  if($null -eq (get-command git -erroraction SilentlyContinue)) {
+    $tmpExeFile =  "$env:TEMP\git-setup.exe"
+    Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe" -OutFile $tmpExeFile
+    ### Start-Process -FilePath $tmpExeFile -ArgumentList "/SILENT", "/NORESTART", "/DIR=C:\Program Files\Git" -Wait -NoNewWindow
+    & $tmpExeFile "/SILENT" "/NORESTART" "/DIR=C:\Program Files\Git" | out-string -stream
+    Remove-item $tmpExeFile -erroraction SilentlyContinue
+    if(0 -ne $lastexitcode) { throw "error. please see above" }
+    $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
+  }
 
 
-if ($null -eq (get-command choco -erroraction SilentlyContinue)) {
-  Set-ExecutionPolicy Bypass -Scope Process -Force
-  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-  iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-}
+  if ($null -eq (get-command choco -erroraction SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  }
 
-if (-not(choco list --lo -r -e dvc)) {
-  choco install dvc -y | out-string -stream
-  if(0 -ne $lastexitcode) { throw "error. please see above" }
-  $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
-}
+  if (-not(choco list --lo -r -e dvc)) {
+    choco install dvc -y | out-string -stream
+    if(0 -ne $lastexitcode) { throw "error. please see above" }
+    $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
+  }
 
-if(-not(test-path "C:\Program Files\GitHub CLI\gh.exe")) {
-  $tmpExeFile =  "$env:TEMP\gh-setup.msi"
-  Invoke-WebRequest -Uri "https://github.com/cli/cli/releases/download/v2.72.0/gh_2.72.0_windows_amd64.msi" -OutFile $tmpExeFile
-  & msiexec.exe /I $tmpExeFile "/quiet" "/norestart" | out-string -stream
-  Remove-item $tmpExeFile -erroraction SilentlyContinue
-  if(0 -ne $lastexitcode) { throw "error. please see above" }
-  $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
+  if(-not(test-path "C:\Program Files\GitHub CLI\gh.exe")) {
+    $tmpExeFile =  "$env:TEMP\gh-setup.msi"
+    Invoke-WebRequest -Uri "https://github.com/cli/cli/releases/download/v2.72.0/gh_2.72.0_windows_amd64.msi" -OutFile $tmpExeFile
+    & msiexec.exe /I $tmpExeFile "/quiet" "/norestart" | out-string -stream
+    Remove-item $tmpExeFile -erroraction SilentlyContinue
+    if(0 -ne $lastexitcode) { throw "error. please see above" }
+    $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
 
-}
+  }
 
-& "C:\Program Files\GitHub CLI\gh.exe" auth status | out-string
-if(1 -eq $lastexitcode) { 
-  & "C:\Program Files\GitHub CLI\gh.exe" auth login --hostname GitHub.com --git-protocol SSH --skip-ssh-key --web
+  & "C:\Program Files\GitHub CLI\gh.exe" auth status | out-string
+  if(1 -eq $lastexitcode) { 
+    & "C:\Program Files\GitHub CLI\gh.exe" auth login --hostname GitHub.com --git-protocol SSH --skip-ssh-key --web
+  }
+} else { 
+  throw "please run in PowerShell 7"
 }
